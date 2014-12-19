b6ac7a4f69aa21d0b75ca68afabb13ad449c5e65
Azat Khuzhin <a3at.mail@gmail.com>
2014-12-19 19:49:51 +0300

    options: initial module

---
+++ src/options.cpp
@@ -0,0 +1,55 @@
+#include "options.h"
+
+#include <cstdlib>
+
+Options::Options(int argc, char **argv)
+    : m_argc(argc)
+    , m_argv(argv)
+{
+    try {
+        addOptions();
+
+        if (getValue("help")) {
+            std::cout << m_options;
+            exit(EXIT_SUCCESS);
+        }
+    } catch (const std::exception &e) {
+        std::cerr << e.what();
+        std::cerr << m_options;
+        exit(EXIT_FAILURE);
+    }
+}
+
+template <class Type>
+Type Options::getValue(const char *optionKey) const
+{
+    if (!m_map.count(optionKey)) {
+        return Type();
+    }
+    return m_map[optionKey].as<Type>();
+}
+bool Options::getValue(const char *optionKey) const
+{
+    return m_map.count(optionKey);
+}
+
+void Options::addOptions()
+{
+    namespace po = boost::program_options;
+
+    static const int style =
+        (po::command_line_style::unix_style ^
+         po::command_line_style::allow_guessing) |
+        po::command_line_style::allow_long_disguise;
+
+    m_options.add_options()
+        ("help,h", "Show help message")
+        ("normalize,n", "Normalize action")
+    ;
+
+    po::store(po::command_line_parser(m_argc, m_argv)
+              .style(style)
+              .options(m_options)
+              .run(), m_map);
+    po::notify(m_map);
+}
