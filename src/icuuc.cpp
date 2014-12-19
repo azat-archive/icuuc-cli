56dd6a1191158b84f1cf87889db3c77000008ce7
Azat Khuzhin <a3at.mail@gmail.com>
2014-12-19 19:50:57 +0300

    icuuc: use options

--- src/icuuc.cpp
+++ src/icuuc.cpp
@@ -3,14 +3,18 @@
 #include <string>
 
 #include "ops.h"
+#include "options.h"
 
 int main(int argc, char **argv)
 {
     std::string buffer;
     Ops ops;
+    Options options(argc, argv);
 
     while (std::getline(std::cin, buffer)) {
-        std::cout << ops.normalize(buffer) << std::endl;
+        if (options.getValue("normalize")) {
+            std::cout << ops.normalize(buffer) << std::endl;
+        }
     }
 
     return EXIT_SUCCESS;
